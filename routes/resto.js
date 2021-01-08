f949e237ddb1238f7748cac20c3f34cae8450c53
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2021-01-08 11:12:36 +0700

    add router.post in resto.js

--- routes/resto.js
+++ routes/resto.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const restoController = require('../controllers/restoController');
 
 router.get('/', restoController.viewResto);
+router.post('/', restoController.addResto);
 
 module.exports = router;
