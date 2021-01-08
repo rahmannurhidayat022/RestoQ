74dbabfe538cbef8757519a79ee8a705496eadf6
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2021-01-08 10:51:05 +0700

    membuat route untuk viewResto di app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect("mongodb://localhost:27017/db_resto", {
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var restoRouter = require('./routes/resto');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/resto', restoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/resto.js
+++ routes/resto.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const restoController = require('../controllers/restoController');
 
-router.get('/', restoController);
+router.get('/', restoController.viewResto);
 
 module.exports = router;
