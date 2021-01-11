a9a327ff2bf11028eafbaff7180cc5be5e05e71d
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2021-01-11 22:00:40 +0700

    create about Controller

---
+++ controllers/aboutController.js
@@ -0,0 +1,13 @@
+const About = require('../models/About');
+
+module.exports = {
+  viewAbout: async (req, res) => {
+    try{
+      const about = await About.find();
+
+      res.render('about', { about, title: Teams });
+    } catch(error) {
+      console.log(error);
+    }
+  }
+};
