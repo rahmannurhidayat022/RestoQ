0c323599098eca519dec0848c10f56e029fb28b6
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2021-01-11 23:17:39 +0700

    about page Done..

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const restoRouter = require('./routes/resto');
 const app = express();
 
 // view engine setup
+app.use('/public', express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -3,9 +3,9 @@ const About = require('../models/About');
 module.exports = {
   viewAbout: async (req, res) => {
     try{
-      const about = await About.find();
-
-      res.render('about', { about, title: "Teams" });
+      const teams = await About.find();
+      
+      res.render('about', { teams, title: "Teams" });
     } catch(error) {
       console.log(error);
     }
